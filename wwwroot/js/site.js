311180ae9f5598df86ede380b11b0fa05f747ac2
Hailey June <hailey.june.b@gmail.com>
2019-03-11 15:49:43 -0700

    Jquery for some new item functionality stuff

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,8 @@
 // Write your Javascript code.
+
+$(document).ready(function () {
+    $(".slidetoggle").click(function () {
+        $(this).next().next().slideToggle(function () {
+            });
+    })
+})
